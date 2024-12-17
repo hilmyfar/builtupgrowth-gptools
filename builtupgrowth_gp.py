348f29302f872337aa076f0af7051b2c6d7d5c14
Hilmy Farras <46394746+hilmyfar@users.noreply.github.com>
2024-12-17 10:17:38 +0700

    Update builtupgrowth_gp.py

--- builtupgrowth_gp.py
+++ builtupgrowth_gp.py
@@ -1,15 +1,15 @@
 """""
 Built-Up Growth Rapid Assessment Geoprocessing Tool's Script by hilmyfar
-https://www.linkedin.com/in/hilmyfarras
+linkedin.com/in/hilmyfarras | github.com/hilmyfar
 
 This script is intended to run with .atbx file as an custom geoprocessing tool in ArcGIS Pro.
 I included custom colormap, but you need to set the path to absolute path in your computer. I doesnt know why but it won't work with relative path.
 
-This script uses NDBI, MNDWI, and modified Index-Based Builtup Index (mIBI) that I personally developed after some intensive research (three weeks *cough *cough).
+This script combines NDBI, MNDWI, and Modified Index-Based Builtup Index (mIBI) that I developed based on Index-Based Built-up Index (IBI) proposed by Xu (2007).
 So yeah, the methods are not scientificly proven (yet). But based on my internal testing, it achieves 85-92% of OA on three urban agglomeration area.
 Basically, this is for fun experiment and learning purpose~
 
-Anyway, you can use it for anything as long as it's not commercial (non-profit) and credited me (attach link to my github/linkedin page). 
+see readme.md files for better explanation of this script.
 """""
 
 import arcpy
